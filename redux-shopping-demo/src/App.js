6d84ef1f42b6624b098f60f18c337e2b26b5fa8a
sahaf <sahaf@softserveinc.com>
2022-09-18 21:24:04 +0300

    [DEV] Advanced Redux

--- redux-shopping-demo/src/App.js
+++ redux-shopping-demo/src/App.js
@@ -1,13 +1,45 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { fetchCartData, sendCartData } from './store/cart-actions';
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const isCartVisible = useSelector(state => state.ui.isCartVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector(state => state.ui.notification)
+
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    cart.changed && dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
+    <Fragment>
+      { notification && <Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message}
+      /> }
+      <Layout>
+        { isCartVisible ? <Cart /> : <Products /> }
+      </Layout>
+    </Fragment>
   );
 }
 
--- redux-shopping-demo/src/components/Cart/Cart.js
+++ redux-shopping-demo/src/components/Cart/Cart.js
@@ -1,15 +1,27 @@
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
 
 const Cart = (props) => {
+  const cartItems = useSelector(state => state.cart.items);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map(cartItem => (
+          <CartItem
+            key={cartItem.id}
+            item={{
+              id: cartItem.id,
+              title: cartItem.name,
+              price: cartItem.price,
+              total: cartItem.totalPrice,
+              quantity: cartItem.quantity
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
