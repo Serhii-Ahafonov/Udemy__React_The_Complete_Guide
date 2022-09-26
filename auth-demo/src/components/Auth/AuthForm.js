1ec35e24cb8ac4a53db7baed888a7308364a4615
sahaf <sahaf@softserveinc.com>
2022-09-26 21:14:10 +0300

    [DEV] added auth configuration

--- auth-demo/src/components/Auth/AuthForm.js
+++ auth-demo/src/components/Auth/AuthForm.js
@@ -55,7 +55,7 @@ const AuthForm = () => {
       }
     }).then(data => {
       const expirationTime = new Date(
-        new Date().getTime() + +data.expiresIn * 1000
+        new Date().getTime() + (+data.expiresIn) * 1000
       );
       authCtx.login(data.idToken, expirationTime.toISOString());
       history.replace('/');
